ac05acc61b38df403f5752c972dacc4006668836
Odion Akhes <akhetuamhenodion@gmail.com>
2023-04-04 13:03:17 +0100

    added state functionality

--- src/Components/Search.jsx
+++ src/Components/Search.jsx
@@ -1,20 +1,24 @@
 import React from 'react'
-
+import { FaSistrix } from "react-icons/fa";
+import mic from '../assets/Google_mic.svg.png'
 const Search = ({handleSubmit,search}) => {
   return (
     <div>
       <form action="" className="search">
-        <span className= ''></span>
+        <span className="">
+          <FaSistrix style={{ color: "#a5aab0" }} />
+        </span>
         <input
           type="text"
           className="search-input"
           value={search}
           onKeyDown={(e) => e.key === "Enter" && handleSubmit(e)}
           onChange={handleSubmit}
-          
         />
-        <img src="" alt="" />
-        <img src="" alt="" />
+        <div style={{display: "flex", gap: "8px"}}>
+          <img src={mic} alt="" />
+          {/* <img src="" alt="" /> */}
+        </div>
       </form>
     </div>
   );
